949890ddbd7ecea50a5576fff6d7a4033acc833e
Vitaliy <v1taliyanisimov@yandex.ru>
2023-03-12 18:50:44 +0300

    making buttons

--- index.js
+++ index.js
@@ -6,30 +6,80 @@ const api = 'http://45.146.166.156:5000/';
 
 const bot = new TelegramApi(token, { polling: true });
 
-let weather = null;
+
 
 bot.on('message', msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+  const text = msg.text;
+  const chatId = msg.chat.id;
 
-if (text === '/weather') {
-        fetch(api)
-            .then((response) => response.json())
-            .then((result) => {
-                if (result) {
-                    weather = result.list[0];
-                    let description = weather.weather[0].description
-                    description = description[0].toUpperCase() + description.slice(1)
+  if (text === '/current_weather') {
+    let weather = null;
 
-                    console.log("weather data updated")
+    fetch(api)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result) {
+          weather = result.list[0];
+          let description = weather.weather[0].description
+          description = description[0].toUpperCase() + description.slice(1)
 
-                    bot.sendMessage(chatId,
-                        `Температура воздуха: ${Math.round(weather.main.temp)}\u00B0\nОщущается как: ${Math.round(weather.main.feels_like)}\u00B0\n${description}`
-                    )
-                } else {
-                    console.log('failed to update weather data')
-                    weather = 'failed to update weather data';
-                }
-            })
-    }
+          console.log("weather data updated")
+
+          bot.sendMessage(chatId,
+            `Температура воздуха: ${Math.round(weather.main.temp)}\u00B0\nОщущается как: ${Math.round(weather.main.feels_like)}\u00B0\n${description}`
+          )
+        } else {
+          console.log('failed to update weather data')
+          weather = 'failed to update weather data';
+        }
+      })
+  }
+
+
+})
+
+
+
+bot.onText(/\/weather/, function (msg, match) {
+  let weatherList = null;
+
+  fetch(api)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result) {
+        let dateList = [];
+        weatherList = result.list;
+        weatherList.forEach(weather => {
+          const date = weather.dt_txt.slice(0, 10)
+          if (!dateList.includes(date)) {
+            dateList.push(date)
+          }
+        });
+
+        let awailibleDaysButtons = []
+
+        dateList.forEach(date => {
+          awailibleDaysButtons.push(
+            [{ text: date, callback_data: date }]
+          )
+        })
+
+        let awailibleDays = {
+          reply_markup: JSON.stringify({
+            inline_keyboard: awailibleDaysButtons
+          })
+        }
+
+        bot.sendMessage(msg.chat.id, 'Выберите дату:', awailibleDays);
+      } else {
+        console.log('failed to update weather data')
+        weather = 'failed to update weather data';
+      }
+    })
+
+
+});
+
+bot.on("callback_query", callbackQuery => {
+  console.log(callbackQuery.data)
 })
