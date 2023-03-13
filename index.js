2ad695b8f0b952ed3e761170f1d11b426e0c6501
Vitaliy <v1taliyanisimov@yandex.ru>
2023-03-13 08:41:52 +0300

    weather by date and time

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
 import TelegramApi from 'node-telegram-bot-api';
 import Fetch from 'node-fetch';
 
-const token = '5727566137:AAHO43LNq7ePTz2lHbtFG6P7R3bg80VPMtI';
-const api = 'http://45.146.166.156:5000/';
+const TOKEN = '5727566137:AAHO43LNq7ePTz2lHbtFG6P7R3bg80VPMtI';
+const API = 'http://45.146.166.156:5000/';
 
-const bot = new TelegramApi(token, { polling: true });
+const bot = new TelegramApi(TOKEN, { polling: true });
 
 
 
-bot.on('message', msg => {
+bot.onText(/\/current_weather/, msg => {
   const text = msg.text;
   const chatId = msg.chat.id;
+  let weather = null;
 
-  if (text === '/current_weather') {
-    let weather = null;
+  fetch(API)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result) {
+        weather = result.list[0];
+        let description = weather.weather[0].description
+        description = description[0].toUpperCase() + description.slice(1)
 
-    fetch(api)
-      .then((response) => response.json())
-      .then((result) => {
-        if (result) {
-          weather = result.list[0];
-          let description = weather.weather[0].description
-          description = description[0].toUpperCase() + description.slice(1)
-
-          console.log("weather data updated")
-
-          bot.sendMessage(chatId,
-            `Температура воздуха: ${Math.round(weather.main.temp)}\u00B0\nОщущается как: ${Math.round(weather.main.feels_like)}\u00B0\n${description}`
-          )
-        } else {
-          console.log('failed to update weather data')
-          weather = 'failed to update weather data';
-        }
-      })
-  }
-
+        bot.sendMessage(chatId,
+          `Погода на ${weather.dt_txt}:\nТемпература воздуха: ${Math.round(weather.main.temp)}\u00B0\nОщущается как: ${Math.round(weather.main.feels_like)}\u00B0\n${description}`
+        )
+      } else {
+        console.log('failed to update weather data')
+        weather = 'failed to update weather data';
+      }
+    })
 
 })
 
 
 
 bot.onText(/\/weather/, function (msg, match) {
-  let weatherList = null;
+  let weatherList = [];
 
-  fetch(api)
+  fetch(API)
     .then((response) => response.json())
     .then((result) => {
       if (result) {
+        //make buttons from availible dates
         let dateList = [];
-        weatherList = result.list;
+        const weatherList = result.list;
         weatherList.forEach(weather => {
           const date = weather.dt_txt.slice(0, 10)
           if (!dateList.includes(date)) {
@@ -75,11 +70,80 @@ bot.onText(/\/weather/, function (msg, match) {
         console.log('failed to update weather data')
         weather = 'failed to update weather data';
       }
-    })
-
+    });
 
 });
 
-bot.on("callback_query", callbackQuery => {
-  console.log(callbackQuery.data)
-})
+bot.on("callback_query", (callbackQuery) => {
+
+  const msg = callbackQuery.message;
+  const callbackData = callbackQuery.data;
+  let timeList = [];
+
+  if (!callbackData.includes(":")) {
+    fetch(API)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result) {
+          //make buttons from availible dates
+          let timeList = [];
+          const weatherList = result.list;
+          weatherList.forEach(weather => {
+            const date = weather.dt_txt.slice(0, 10)
+            const time = weather.dt_txt
+            if (!timeList.includes(time) && (date === callbackData)) {
+
+              timeList.push(time)
+            }
+          });
+
+          let awailibleTimeButtons = []
+
+          timeList.forEach(time => {
+
+            awailibleTimeButtons.push(
+              [{ text: time, callback_data: time }]
+            )
+          })
+
+          let awailibleTime = {
+            reply_markup: JSON.stringify({
+              inline_keyboard: awailibleTimeButtons
+            })
+          }
+          bot.sendMessage(msg.chat.id, 'Выберите время:', awailibleTime);
+        } else {
+          console.log('failed to update weather data')
+          weather = 'failed to update weather data';
+        }
+      });
+  } else {
+    fetch(API)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result) {
+          let weather = null;
+          const weatherList = result.list;
+          weatherList.forEach(weatherData => {
+            const date = weatherData.dt_txt
+            if (date === callbackData) {
+              weather = weatherData
+            }
+          });
+          if (weather) {
+            let description = weather.weather[0].description
+            description = description[0].toUpperCase() + description.slice(1)
+            bot.sendMessage(msg.chat.id,
+              `Погода на ${weather.dt_txt}:\nТемпература воздуха: ${Math.round(weather.main.temp)}\u00B0\nОщущается как: ${Math.round(weather.main.feels_like)}\u00B0\n${description}`
+            )
+          } else {
+            bot.sendMessage(msg.chat.id, "Не найдено")
+          }
+
+        } else {
+          console.log('failed to update weather data')
+          weather = 'failed to update weather data';
+        }
+      });
+  }
+});
